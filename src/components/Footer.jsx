2ecd83d95084a18139038abd58bd17113e09397a
Esteban Lorca <132632580+enlorca@users.noreply.github.com>
2024-11-14 18:58:42 -0300

    feat: :sparkles: ADD Se agrega footer
    ADD Se agrega footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-icons">
+          <a
+            href="https://facebook.com"
+            className="social-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-facebook fa-2x"></i>
+          </a>
+          <a
+            href="https://instagram.com"
+            className="social-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram fa-2x"></i>
+          </a>
+          <a href="mailto:contact@neidashop.com" className="social-icon">
+            <i className="far fa-envelope fa-2x"></i>
+          </a>
+          <a href="tel:+1234567890" className="social-icon">
+            <i className="fas fa-phone fa-2x"></i>
+          </a>
+        </div>
+        <div className="footer-copyright">
+          <p>© 2024 Copyright: Codelium</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
